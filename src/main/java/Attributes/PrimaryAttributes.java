b4df8b939374b1676c3dfc3e0a79be0dd4e59031
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-17 22:03:25 +0100

    Fix(attributes): created attributes

---
+++ src/main/java/Attributes/PrimaryAttributes.java
@@ -0,0 +1,46 @@
+package Attributes;
+
+public class PrimaryAttributes {
+    int strength;
+    int dexterity;
+    int intelligence;
+
+    public PrimaryAttributes(int strength, int dexterity, int intelligence) {
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.intelligence = intelligence;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getDexterity() {
+        return dexterity;
+    }
+
+    public void setDexterity(int dexterity) {
+        this.dexterity = dexterity;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(int intelligence) {
+        this.intelligence = intelligence;
+    }
+
+    @Override
+    public String toString() {
+        return "Attributes { " +
+                "strength = " + strength +
+                ", dexterity = " + dexterity +
+                ", intelligence = " + intelligence +
+                " }";
+    }
+}
---
+++ src/main/java/Attributes/SecondaryAttributes.java
@@ -0,0 +1,31 @@
+package Attributes;
+
+public class SecondaryAttributes {
+    public int Health;
+    public int RatingArmor;
+    public int Resistance;
+
+    public int getHealth() {
+        return Health;
+    }
+
+    public void setHealth(int health) {
+        Health = health;
+    }
+
+    public int getRatingArmor() {
+        return RatingArmor;
+    }
+
+    public void setRatingArmor(int ratingArmor) {
+        RatingArmor = ratingArmor;
+    }
+
+    public int getResistance() {
+        return Resistance;
+    }
+
+    public void setResistance(int resistance) {
+        Resistance = resistance;
+    }
+}
