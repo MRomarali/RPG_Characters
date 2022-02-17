1d15a4910673193198dd3a69e2f9cb039177fa17
OmarAbdiAli <omarabdi.ali@se.experis.com>
2022-02-17 21:47:16 +0100

    Fix(weaponattribute): created weapon attribute

---
+++ src/main/java/Attributes/WeaponAttributes.java
@@ -0,0 +1,22 @@
+package Attributes;
+
+public class WeaponAttributes {
+    public int Damage;
+    public double AttackSpeed;
+
+    public int getDamage() {
+        return Damage;
+    }
+
+    public void setDamage(int damage) {
+        Damage = damage;
+    }
+
+    public double getAttackSpeed() {
+        return AttackSpeed;
+    }
+
+    public void setAttackSpeed(double attackSpeed) {
+        AttackSpeed = attackSpeed;
+    }
+}
